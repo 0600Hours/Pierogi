432a4185777e57c6a2d443e29fd0c58b3fcb7bad
Thomas' Pendley <tpendley360@gmail.com>
2022-07-06 15:23:37 -0700

    init handlers folder, switch to yaml

---
+++ pierogi/pierogi.py
@@ -0,0 +1,35 @@
+import logging
+from typing import List
+from telegram.ext import ApplicationBuilder, BaseHandler
+import yaml
+
+# logging
+logging.basicConfig(
+    format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level = logging.INFO
+)
+
+# core bot class
+class Pierogi:
+    def __init__(self, config, handlers):
+        self.app = ApplicationBuilder().token(config['BOT_TOKEN']).build()
+        self.app.addHandlers(handlers)
+
+    def run(self):
+        self.app.run_polling()
+
+if __name__ == '__main__':
+    from pierogi.handlers import handlers
+
+    logging.info('began running')
+
+    # get config
+    with open('config.yaml', 'r') as stream:
+        try:
+            config = yaml.safe_load(stream)
+        except yaml.YAMLError as e:
+            logging.error(e)
+            quit()
+    
+    pierogi = Pierogi(config, handlers)
+    pierogi.run()
--- src/config.py
+++
@@ -1,4 +0,0 @@
-#NOTE TO SELF: this is marked as ignored in git. run git update-index --no-assume-unchanged src/config.py before you commit changes
-
-BOT_NAME='@pierogi_quote_bot'
-TOKEN='register with @BotFather for a token'
