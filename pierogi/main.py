7c2399aaf7453be94c586fea2ee8df7e98ac3a1d
Thomas' Pendley <tpendley360@gmail.com>
2022-07-06 17:33:06 -0700

    holy shit it runs

--- pierogi/main.py
+++ pierogi/main.py
@@ -1,7 +1,8 @@
 import logging
-from telegram.ext import ApplicationBuilder
-import yaml
 import handlers
+import os
+import yaml
+from telegram.ext import ApplicationBuilder
 
 # logging
 logging.basicConfig(
@@ -10,11 +11,15 @@ logging.basicConfig(
 )
 
 
+# constants
+CONFIG_FILENAME = 'config.yaml'
+
+
 # core bot class
 class PierogiCore:
     def __init__(self, config, handlers):
         self.app = ApplicationBuilder().token(config['BOT_TOKEN']).build()
-        self.app.addHandlers(handlers)
+        self.app.add_handlers(handlers)
 
     def run(self):
         self.app.run_polling()
@@ -24,12 +29,12 @@ if __name__ == '__main__':
     logging.info('began running')
 
     # get config
-    with open('config.yaml', 'r') as stream:
+    with open(os.path.join('data', CONFIG_FILENAME), 'r') as stream:
         try:
             config = yaml.safe_load(stream)
         except yaml.YAMLError as e:
             logging.error(e)
             quit()
 
-    pierogiCore = PierogiCore(config, handlers)
+    pierogiCore = PierogiCore(config, handlers.handlers)
     pierogiCore.run()
